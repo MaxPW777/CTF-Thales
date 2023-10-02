738189d38ca3d63178690750c1ce451b9f906bdb
Maximilian <61150071+MaxPW777@users.noreply.github.com>
2023-10-02 23:17:43 +0200

    main site basics finished

---
+++ js/main.js
@@ -0,0 +1,34 @@
+import { generateWindowList } from './listGen.js';
+
+class windowItem {
+  constructor(name, image, price) {
+    this.name = name;
+    this.image = image;
+    this.price = price;
+  }
+}
+
+const mainContent = document.querySelector('main');
+const numberOfImages = 6;
+
+const itemArray = generateWindowList(numberOfImages);
+
+mainContent.innerHTML = itemArray.map(item => {
+    return `<div class="windowItem" onClick="focusWindow(this)">
+                <img src="/images/${item.image}" alt="${item.name}">
+                <div class="windowDescription">${item.name}</div>
+                <div class="windowPrice">${item.price}</div>
+            </div>`
+}).join('');
+
+function focusWindow(element) {
+
+    Array.from(document.getElementsByClassName('windowItem')).forEach(elements => {
+  elements.classList.toggle('hidden');
+});
+    element.classList.toggle('focus');
+    element.classList.toggle('hidden');
+    document.querySelector("main").classList.toggle('focus');
+    console.log("this is blurred")
+}
+
